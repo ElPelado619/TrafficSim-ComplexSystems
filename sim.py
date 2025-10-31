677d98ac2a740725e9405e357919be2187dae3b4
ElPelado619 <agustinyornet@gmail.com>
2025-10-31 15:50:42 -0300

    Update sim.py to generate map without pedestrian walks

--- sim.py
+++ sim.py
@@ -1,13 +1,13 @@
 import osmnx as ox
 
-# Cargar el grafo de calles directamente desde tu archivo .osm
-# Esto crea una representación matemática de tu mapa
-graph = ox.graph_from_xml("data/map.osm")
+polygon = -68.86471, -32.90063, -68.82711, -32.88276
+
+graph = ox.graph_from_bbox(polygon, network_type='drive')
 
 # Guardar el grafo en un archivo GraphML
-ox.save_graphml(graph, "data/map.graphml")
+ox.save_graphml(graph, "data/independencia.graphml")
 
-print("Grafo guardado en data/map.graphml")
+print("Grafo guardado en data/independencia.graphml")
 
 # Opcional: Visualizar el grafo
 ox.plot_graph(graph)
